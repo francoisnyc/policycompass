e220222c9d3096ac7a4c0919ace3e616e59d648a
ashley williams <heyashleyashley@gmail.com>
2013-03-08 11:32:39 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Policycompass::Application.config.secret_token = 'd7ddaed9301705bd471b81549248560e61ded2a7ffbdf314983ee5492bfbdba69043e45a1c77ccfb5fdb597a380f53c5d10384d9c932bbde92ff1738d9b067cf'
