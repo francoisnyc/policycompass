7458edc9a32ec62efec640f48626a80fd944e9e9
ashley williams <heyashleyashley@gmail.com>
2013-03-16 13:41:42 -0400

    add validate for name and email to voter model

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -2,4 +2,7 @@ class Voter < ActiveRecord::Base
   has_many :voter_answers, :questions through: :voter_answers
 
   attr_accessible :email, :name, :formal_affiliation, :location
+
+  validates :name, presence: true
+  validates :email, presence: true
 end
