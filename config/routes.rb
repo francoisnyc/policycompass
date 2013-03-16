439396443fa4ac36e0098f7e253d896b7f26f5b6
ashley williams <heyashleyashley@gmail.com>
2013-03-16 18:21:51 -0400

    tweak forms a bit

--- config/routes.rb
+++ config/routes.rb
@@ -34,10 +34,6 @@ Policycompass::Application.routes.draw do
     resources :candidate_answers
   end
 
-  match "auth/:service"          => "auth#service"
-  match "auth/:service/callback" => "auth#callback"
-  match "logout"                 => "auth#logout"
-
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
