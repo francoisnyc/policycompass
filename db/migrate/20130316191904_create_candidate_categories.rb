3f0bf26bef97f4a4c871db26396170c0ec338740
Michael Hansen <modality2@gmail.com>
2013-03-16 15:28:44 -0400

    non-null those ID columns

--- db/migrate/20130316191904_create_candidate_categories.rb
+++ db/migrate/20130316191904_create_candidate_categories.rb
@@ -1,8 +1,8 @@
 class CreateCandidateCategories < ActiveRecord::Migration
   def change
     create_table :candidate_categories do |t|
-      t.integer :candidate_id
-      t.integer :category_id
+      t.integer :candidate_id, :null => false
+      t.integer :category_id, :null => false
 
       t.timestamps
     end
