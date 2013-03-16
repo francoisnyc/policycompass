21d89f9d92a0bcb69d014484340abe2ffe40b92e
Michael Hansen <modality2@gmail.com>
2013-03-16 13:25:19 -0400

    add candidate controllers

---
+++ app/controllers/candidate_controller.rb
@@ -0,0 +1,28 @@
+class CandidatesController < ApplicationController
+  before_filter :require_candidate, :except => [:new, :create]
+
+  def new
+    @candidate = Candidate.new
+  end
+
+  def create
+    @candidate = Candidate.create(params[:candidate])
+  end
+
+  def edit
+  end
+
+  def update
+    @candidate.update_attributes(params[:candidate])
+    redirect_to candidate_path(@candidate)
+  end
+
+  def show
+  end
+
+  private
+
+  def require_candidate
+    @candidate = Candidate.find(params[:id])
+  end
+end
