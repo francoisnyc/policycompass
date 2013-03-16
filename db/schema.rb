23cb0ec5cc992d3d27c614c26cedb862326d4363
ashley williams <heyashleyashley@gmail.com>
2013-03-16 13:12:29 -0400

    add voter model to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130316162516) do
+ActiveRecord::Schema.define(:version => 20130316164112) do
 
   create_table "orgs", :force => true do |t|
     t.string   "name"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(:version => 20130316162516) do
     t.string   "question"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "voters", :force => true do |t|
+    t.string   "email"
+    t.string   "name"
+    t.string   "formal_affiliation"
+    t.string   "location"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
   end
 
 end
