0688e3c5780b57a8da4ee3443556e2c71886a04e
ashley williams <heyashleyashley@gmail.com>
2013-03-17 16:50:22 -0400

    tweaks to index and nav to prep for login stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
 
   attr_accessible :email
 
+  before_save { |user| user.email = email.downcase }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
                     format: { with: VALID_EMAIL_REGEX },
