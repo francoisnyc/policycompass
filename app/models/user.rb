e3cea75418d9561b3ca13a4e022eabc9e61bbc4d
ashley williams <heyashleyashley@gmail.com>
2013-03-17 16:55:09 -0400

    add secure password to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
   has_one :voter
   has_one :candidate
   has_one :org
+  has_secure_password
 
-  attr_accessible :email
+  attr_accessible :email, :password, :password_confirmation
 
   before_save { |user| user.email = email.downcase }
 
@@ -11,6 +12,10 @@ class User < ActiveRecord::Base
   validates :email, presence: true,
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
 
   def voter?
     voter.present?
