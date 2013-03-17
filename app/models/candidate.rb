6b8581c3839c93bdd94ab077750145ca28443798
ashley williams <heyashleyashley@gmail.com>
2013-03-17 15:26:18 -0400

    tweak validations for orgs, candidates, and voters

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -8,7 +8,10 @@ class Candidate < ActiveRecord::Base
 
   attr_accessible :formal_affiliation, :name, :website, :email
 
-  validates :name,  presence: true, length: { maximum: 50 }
+  validates :name,  presence: true, 
+  					length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true,
+  					format: { with: VALID_EMAIL_REGEX },
+  					uniqueness: { case_sensitive: false }
 end
--- app/models/org.rb
+++ app/models/org.rb
@@ -4,8 +4,10 @@ class Org < ActiveRecord::Base
 
   attr_accessible :contact_name, :location, :name, :phone_number, :website
 
-  validates :name, :location, :contact_name, :website, :phone_number, presence: true
-  validates :name,  presence: true, length: { maximum: 50 }
+  validates :name,  presence: true, 
+  					length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true,
+  					format: { with: VALID_EMAIL_REGEX },
+  					uniqueness: { case_sensitive: false }
 end
